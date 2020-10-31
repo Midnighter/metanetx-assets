c4c4172bc010b9fcb732240414ff67884cdec8bd
Midnighter <midnighter@posteo.net>
2020-10-31 15:07:46 +0100

    docs: add missing docstrings

--- tests/test_unit/etl/test_reaction.py
+++ tests/test_unit/etl/test_reaction.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Test equation parsing for reactions."""
+
+
 from pathlib import Path
 
 import pytest
@@ -10,7 +28,7 @@ with (Path(__file__).parent / "data" / "sample_equations.txt").open() as handle:
 
 
 def test_parsing_empty_reaction() -> None:
-    """"""
+    """Expect that an empty equation can be parsed."""
     assert EquationParser.parse_participants("=", {}, {}) == []
 
 
